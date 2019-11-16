6b37d6fbb4d711297a87da9a6c1ed946835a3f5c
Nisha <hungnguyenw420@gmail.com>
2019-11-16 09:53:57 +0700

    Add SinhVien Model

--- src/Model/SinhVien.java
+++ src/Model/SinhVien.java
@@ -4,11 +4,67 @@
  * and open the template in the editor.
  */
 package Model;
-
+import java.util.Date;
+import java.util.StringTokenizer;
 /**
  *
  * @author LEGION
  */
 public class SinhVien {
-    
+
+    public SinhVien() {
+    }
+
+    public SinhVien(Name tenSv, int maSv, Date dob, String address, String sdt, String gioitinh) {
+        this.tenSv = tenSv;
+        this.maSv = maSv;
+        this.dob = dob;
+        this.address = address;
+        this.sdt = sdt;
+        this.gioitinh = gioitinh;
+    }
+    private Name tenSv;
+    private int maSv;
+    private Date dob;
+    private String address;
+    private String sdt;
+    private String gioitinh;
+}
+class Name{
+
+    public Name() {
+    }
+
+    public Name(String ho, String ten, String dem) {
+        this.ho = ho;
+        this.ten = ten;
+        this.dem = dem;
+    }
+
+
+    private String ho,ten,dem;
+
+    public String getHo() {
+        return ho;
+    }
+
+    public void setHo(String ho) {
+        this.ho = ho;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getDem() {
+        return dem;
+    }
+
+    public void setDem(String dem) {
+        this.dem = dem;
+    }
 }
