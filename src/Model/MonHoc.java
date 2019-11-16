18af491f3cb7b7137ec069f23d743e368977e7b2
Nisha <hungnguyenw420@gmail.com>
2019-11-16 16:20:42 +0700

    update model Monhoc

--- src/Model/MonHoc.java
+++ src/Model/MonHoc.java
@@ -12,10 +12,16 @@ import java.io.Serializable;
  * @author LEGION
  */
 public class MonHoc implements Serializable{
+
+
     private int maMon;
     private String tenMon;
     private int sotinchi;
     private int maGv;
+    private String phong;
+    private String thoigian;
+    private String tietbd;
+    private String ngay;
 
     public MonHoc(int maMon, String tenMon, int sotinchi, int maGv) {
         this.maMon = maMon;
@@ -57,5 +63,47 @@ public class MonHoc implements Serializable{
 
     public void setMaGv(int maGv) {
         this.maGv = maGv;
+    }
+        public String getPhong() {
+        return phong;
+    }
+
+    public void setPhong(String phong) {
+        this.phong = phong;
+    }
+
+    public String getThoigian() {
+        return thoigian;
+    }
+
+    public void setThoigian(String thoigian) {
+        this.thoigian = thoigian;
+    }
+
+    public String getTietbd() {
+        return tietbd;
+    }
+
+    public void setTietbd(String tietbd) {
+        this.tietbd = tietbd;
+    }
+
+    public String getNgay() {
+        return ngay;
+    }
+
+    public void setNgay(String ngay) {
+        this.ngay = ngay;
+    }
+
+    public MonHoc(int maMon, String tenMon, int sotinchi, int maGv, String phong, String thoigian, String tietbd, String ngay) {
+        this.maMon = maMon;
+        this.tenMon = tenMon;
+        this.sotinchi = sotinchi;
+        this.maGv = maGv;
+        this.phong = phong;
+        this.thoigian = thoigian;
+        this.tietbd = tietbd;
+        this.ngay = ngay;
     }
 }
