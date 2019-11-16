73e7bdd2a1d30082b875a7d3ee4853d316dec066
Nisha <hungnguyenw420@gmail.com>
2019-11-16 09:59:17 +0700

    Add  Model

--- src/Model/MonHoc.java
+++ src/Model/MonHoc.java
@@ -10,5 +10,50 @@ package Model;
  * @author LEGION
  */
 public class MonHoc {
-    
+    private int maMon;
+    private String tenMon;
+    private int sotinchi;
+    private int maGv;
+
+    public MonHoc(int maMon, String tenMon, int sotinchi, int maGv) {
+        this.maMon = maMon;
+        this.tenMon = tenMon;
+        this.sotinchi = sotinchi;
+        this.maGv = maGv;
+    }
+
+    public MonHoc() {
+    }
+
+    public int getMaMon() {
+        return maMon;
+    }
+
+    public void setMaMon(int maMon) {
+        this.maMon = maMon;
+    }
+
+    public String getTenMon() {
+        return tenMon;
+    }
+
+    public void setTenMon(String tenMon) {
+        this.tenMon = tenMon;
+    }
+
+    public int getSotinchi() {
+        return sotinchi;
+    }
+
+    public void setSotinchi(int sotinchi) {
+        this.sotinchi = sotinchi;
+    }
+
+    public int getMaGv() {
+        return maGv;
+    }
+
+    public void setMaGv(int maGv) {
+        this.maGv = maGv;
+    }
 }
